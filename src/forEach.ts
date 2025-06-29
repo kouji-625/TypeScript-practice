8daac79216af7f8669c35bffd254635de1a674d3
kouji-625 <uru3022mys@gmail.com>
2025-06-29 22:49:50 +0900

    Update forEach.ts

--- src/forEach.ts
+++ src/forEach.ts
@@ -2,15 +2,13 @@ const numbers4 = [1, 2, 3];
 const result4 = numbers4.forEach(n => {
   const doubled = n * 2;
   console.log(doubled);
-  // でも return してないので値は戻らない
 });
 
 console.log(result4); // undefined
 
 export {};
 
-// 以下の数値配列の合計を計算してください。
-// reduceは使わず、forEachで合計値を出すこと。
+
 
 const prices = [100, 200, 300];
 let total = 0;
